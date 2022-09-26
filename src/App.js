b064e5006b29ab4d788fcb8604858d95b5fd1d54
Nattakit​ Rattanakeha <nattakit.rattanakeha@gmail.com>
2022-09-26 22:56:38 +0700

    feat:create ui&style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <h1>TodoList App</h1>
+      <form className="group">
+        <div className="form-control">
+          <input type="text" className="text-input"></input>
+          <button type="submit" className="submit-btn">
+            เพิ่มข้อมูล
+          </button>
+        </div>
+      </form>
+    </section>
   );
 }
 
