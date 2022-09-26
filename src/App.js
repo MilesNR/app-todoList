4d114d1d963cbfe84c2d09484c87d5ede0cd2def
Nattakit​ Rattanakeha <nattakit.rattanakeha@gmail.com>
2022-09-27 00:20:12 +0700

    feat:add event for state input and listdata

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const submitData = (event) => {
+    event.preventDefault();
+    const newItem = { id: uuidv4(), title: name };
+    setList([...list, newItem]);
+    setName("");
+  };
   return (
     <section className="container">
       <h1>TodoList App</h1>
-      <form className="group">
+      <form className="form-group" onSubmit={submitData}>
         <div className="form-control">
-          <input type="text" className="text-input"></input>
+          <input
+            type="text"
+            className="text-input"
+            onChange={(event) => setName(event.target.value)}
+            value={name}
+          ></input>
           <button type="submit" className="submit-btn">
             เพิ่มข้อมูล
           </button>
